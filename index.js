def116b2ff70a14174a6b77904d5c704f6a293b2
Brandon Mason <q@bitmage.net>
2019-02-20 10:30:39 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,29 @@
+// just a testing library
 var expect = require('chai').expect
 
+// input we'll be using, and expected output
 var input = '"case_clamshell,pilot_in,wrap_in"'
 var output = 'case_clamshell+pilot_in+wrap_in'
 
+// rewrite function that performs the transformation
 function rewrite (text) {
+  
+  // regex just looks for text surrounded by quotes
   var match = text.match(/^"(.+)"$/)
+  
+  // if we got a match, and the inner parens matched
   if (match && match[1]) {
+    
+    // replace all the commas with pluses
     return match[1].replace(/,/g, '+')
   }
+  
+  // if we didn't get a match just return what we started with
   return text
 }
 
+// test our function
 expect(rewrite(input)).equals(output)
+
+// if expect didn't blow up then we're ok :-)
 console.log('ok')
